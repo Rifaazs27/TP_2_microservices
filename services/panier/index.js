f4b22ec6bbecb7eccc159dfc08d032f0e7ea6c2d
Rifaazs27 <mougammadouzaafir@gmail.com>
2026-05-05 11:50:58 +0200

    Update index.js

--- services/panier/index.js
+++ services/panier/index.js
@@ -1,6 +1,39 @@
 const express = require('express');
+const client = require('prom-client');
 const app = express();
 app.use(express.json());
+
+// --- CONFIGURATION PROMETHEUS ---
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total HTTP requests',
+  labelNames: ['method', 'route', 'status_code'],
+});
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests',
+  labelNames: ['method', 'route', 'status_code'],
+});
+
+register.registerMetric(httpRequestCounter);
+register.registerMetric(httpRequestDuration);
+
+app.use(express.json());
+
+// Middleware instrumentation
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+  res.on('finish', () => {
+    const route = req.route ? req.route.path : req.path;
+    httpRequestCounter.labels(req.method, route, res.statusCode).inc();
+    end({ method: req.method, route, status_code: res.statusCode });
+  });
+  next();
+});
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
@@ -44,7 +77,10 @@ app.delete('/cart', (req, res) => {
   res.json({ message: 'Panier vidé' });
 });
 
-// TODO ETUDIANT : exposer GET /metrics
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 const PORT = 3002;
 app.listen(PORT, () => console.log(`[panier] http://localhost:${PORT}`));
